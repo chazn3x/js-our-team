4c6a0bbb0ad6fb30e9be451665d710becbca9084
Giovanni <gio.a.98@gmail.com>
2021-11-20 19:54:39 +0100

    fine bonus, sviluppo funzioni e tasto elimina card

--- js/script.js
+++ js/script.js
@@ -2,52 +2,85 @@ let team = [
     {
         "name": "Wayne Barnett",
         "role": "Founder & CEO",
-        "img": "img/wayne-barnett-founder-ceo.jpg"
+        "img": "wayne-barnett-founder-ceo.jpg"
     },
     {
         "name": "Angela Caroll",
         "role": "Chief Editor",
-        "img": "img/angela-caroll-chief-editor.jpg"
+        "img": "angela-caroll-chief-editor.jpg"
     },
     {
         "name": "Walter Gordon",
         "role": "Office Manager",
-        "img": "img/walter-gordon-office-manager.jpg"
+        "img": "walter-gordon-office-manager.jpg"
     },
     {
         "name": "Angela Lopez",
         "role": "Social Media Manager",
-        "img": "img/angela-lopez-social-media-manager.jpg"
+        "img": "angela-lopez-social-media-manager.jpg"
     },
     {
         "name": "Scott Estrada",
         "role": "Developer",
-        "img": "img/scott-estrada-developer.jpg"
+        "img": "scott-estrada-developer.jpg"
     },
     {
         "name": "Barbara Ramos",
         "role": "Graphic Designer",
-        "img": "img/barbara-ramos-graphic-designer.jpg"
+        "img": "barbara-ramos-graphic-designer.jpg"
     }
 ];
-const teamHTML = document.querySelector(".team-container");
-let items = '';
-for (let i = 0; i < team.length; i++) {
-    const imgHTML = team[i].img
-    const nameHTML = team[i].name
-    const roleHTML = team[i].role
-    items += 
-    `<div class="team-card">
-    <div class="card-image">
-      <img
-        src="${imgHTML}"
-        alt="${nameHTML}"
-      />
-    </div>
-    <div class="card-text">
-      <h3>${nameHTML}</h3>
-      <p>${roleHTML}</p>
-    </div>
-    </div>`;
+
+createCards(); // richiamo della funzione per creare le card
+
+// aggiunta nuova card +++++++++
+document.getElementById("addMemberButton").addEventListener("click", function () {
+    const newName = document.getElementById("name").value; // input nome
+    const newRole = document.getElementById("role").value; // input ruolo
+    const newImg = document.getElementById("image").value; // input immagine.jpg
+    const newMember = { // nuovo oggetto con gli input inseriti
+        "name": newName,
+        "role": newRole,
+        "img": newImg
+    }
+    team.push(newMember); // inserimento nell'array
+    createCards(); // richiamo della funzione per creare le cards con il nuovo membro
+});
+
+// funzioni ++++++++
+
+// funzione per creare le cards
+function createCards() {
+    const teamHTML = document.querySelector(".team-container");
+    let items = '';
+    for (let i = 0; i < team.length; i++) {
+        const imgHTML = team[i].img;
+        const nameHTML = team[i].name;
+        const roleHTML = team[i].role;
+        items += 
+        `<div class="team-card">
+        <div class="delete">X</div>
+        <div class="card-image">
+          <img
+            src="img/${imgHTML}"
+            alt="${nameHTML}"/>
+        </div>
+        <div class="card-text">
+          <h3>${nameHTML}</h3>
+          <p>${roleHTML}</p>
+        </div>
+        </div>`;
+    };
+    teamHTML.innerHTML = items;
+    deleteCard(); // richiamo funzione per poter eliminare le cards
 }
-teamHTML.innerHTML = items;
+// funzione per cancellare una card (elimina anche l'oggetto dall'array)
+function deleteCard() {
+    const deleteBtn = document.getElementsByClassName("delete");
+    for (let i = 0; i < deleteBtn.length; i++) {
+        deleteBtn[i].addEventListener("click", function () {
+            team.splice(i, 1); // rimuove l'oggetto dall'array
+            createCards(); // ricrea le cards senza quella cancellata
+        });
+    }
+}
